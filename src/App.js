c8133998a95c80622b19946a602950c4313ecf13
Eziz-pentester <ezizatabayew1598@gmail.com>
2025-07-03 13:57:48 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
+import React, { useEffect } from 'react';
 import './App.css';
 
-const tg = window.Telegram.WebApp;
-
+const tg = window.Telegram?.WebApp || {
+  ready: () => console.log("tg.ready() mocked"),
+  close: () => console.log("tg.close() mocked"),
+};
 
 function App() {
-   // useEffect(() => {
-    //  tg.ready();
-   // }, [])
-    
-    const onClose = () => {
-      tg.close()
-    }
+  useEffect(() => {
+    tg.ready();
+  }, []);
+
+  const onClose = () => {
+    tg.close();
+  };
 
   return (
     <div className="App">
       Welcome to the Web App!
-         <button onClick={onClose}>Close</button>
+      <button onClick={onClose}>Close</button>
     </div>
   );
 }
