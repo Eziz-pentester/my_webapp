62519c00849427409e37952ce24278610293e937
Eziz-pentester <ezizatabayew1598@gmail.com>
2025-07-05 14:46:15 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React, { useEffect } from 'react';
 import './App.css';
-
+import { useTelegram } from './components/hooks/useTelegram'; 
 const tg = window.Telegram?.WebApp || {
   ready: () => console.log("tg.ready() mocked"),
   close: () => console.log("tg.close() mocked"),
 };
 
 function App() {
+  const {onToggleButton, tg} = useTelegram();
   useEffect(() => {
     tg.ready();
   }, []);
 
-  const onClose = () => {
-    tg.close();
-  };
+  
 
   return (
     <div className="App">
-      Welcome to the Web App!
-      <button onClick={onClose}>Close</button>
+      
+      <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
