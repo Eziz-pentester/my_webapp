0ee3c90c97eff415772e816be82cf0226dc20004
Eziz-pentester <ezizatabayew1598@gmail.com>
2025-07-05 14:54:26 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { useEffect } from 'react';
 import './App.css';
 import { useTelegram } from './components/hooks/useTelegram'; 
-const tg = window.Telegram?.WebApp || {
-  ready: () => console.log("tg.ready() mocked"),
-  close: () => console.log("tg.close() mocked"),
-};
+
 
 function App() {
   const {onToggleButton, tg} = useTelegram();
   useEffect(() => {
     tg.ready();
   }, []);
-
-  
 
   return (
     <div className="App">
