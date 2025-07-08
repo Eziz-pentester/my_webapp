d1d7895fe49a0934b405ae5c2a174c49dd4101ad
Eziz-pentester <ezizatabayew1598@gmail.com>
2025-07-08 14:06:28 +0500

    Initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Button from "../Button/Button";
+import { useTelegram } from "../hooks/useTelegram";
+const Header = () => {
+    const{tg,user,onClose} = useTelegram();
+    
+
+    return (
+        <div className={'header'}>
+            <Button onClick={onClose}>Close</Button>
+            <span className={'username'}>
+                {tg.initDataUnsafe?.user?.first_name || "User"}
+            </span>
+        </div>
+    );
+};
+
+export default Header;
