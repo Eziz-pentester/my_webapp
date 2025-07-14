73077f23e9b5eba93bd62a53d4fea172977bde2c
Eziz-pentester <ezizatabayew1598@gmail.com>
2025-07-14 15:29:35 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Button } from './components/Button/Button';
 import { Header } from './components/Header/Header';
 import { Route, Routes } from 'react-router-dom';
 import { ProductList } from './components/ProductList/ProductList';
-import { Form } from './components/Form/Form';
+import { Form } from './components/Form';
 
 function App() {
   const {onToggleButton, tg} = useTelegram();
