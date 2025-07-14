c5daf9297e9c6a8c23694aa7de4097475375b29c
Eziz-pentester <ezizatabayew1598@gmail.com>
2025-07-14 15:19:09 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { useEffect } from 'react';
 import './App.css';
 import { useTelegram } from './components/hooks/useTelegram'; 
-
+import { Button } from './components/Button/Button';  
+import { Header } from './components/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import { ProductList } from './components/ProductList/ProductList';
+import { Form } from './components/Form/Form';
 
 function App() {
   const {onToggleButton, tg} = useTelegram();
@@ -11,8 +15,11 @@ function App() {
 
   return (
     <div className="App">
-      
-      <button onClick={onToggleButton}>toggle</button>
+      <Header/>
+      <Routes>
+        <Route index element={<ProductList />} />
+        <Route path={'form'} element={<Form />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/form/Form.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect } from "react"
+import './Form.css';
+import { useTelegram } from "../hooks/useTelegram";
+import { Button } from "../Button/Button";
+
+const { tg } = useTelegram();
+
+
+useEffect(() => {
+    tg.MainButton.setParams({text: "Отправить данные"});
+    });
+
+
+useEffect(() => {
+    if (!country || !city) {
+        tg.MainButton.hide();
+    } else {
+        tg.MainButton.show();
+    }
+}, [country, city]);
+
+const Form = () => {
+    const [country, setCountry] = React.useState('');
+    const [city, setCity] = React.useState('');
+    const [type, setType] = React.useState('physical');
+
+const onChangeCountry = (e) => {
+    setCountry(e.target.value);
+}    
+
+const onChangeCity = (e) => {
+    setCity(e.target.value);
+}
+
+const onChangeType = (e) => {
+    setType(e.target.value);
+}
+
+    return (
+        <div className="form">
+            <h3>Введите ваши данные</h3>
+            <input classname={'input'} type= "text" placeholder={country} onChange={onChangeCountry}/>
+            <input classname={'input'} type= "text" placeholder={city} onChange={onChangeCity}/>
+            <select className={'select'} value={type} onChange={onChangeType}>
+                <option value={'physical'}>Физ. лицо</option>
+                <option value={'legal'}>Юр. лицо</option>
+                
+            </select>
+
+        </div>
+    );
+}
+
+export default Form;
