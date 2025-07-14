fd1983ecfb837fc69e214b7929e84b0f0513db0f
Eziz-pentester <ezizatabayew1598@gmail.com>
2025-07-14 15:49:15 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, { useEffect } from 'react';
 import './App.css';
 import { useTelegram } from './components/hooks/useTelegram'; 
 import { Button } from './components/Button/Button';  
-import { Header } from './components/Header/Header';
+import  Header  from './components/Header/Header';
 import { Route, Routes } from 'react-router-dom';
-import { ProductList } from './components/ProductList/ProductList';
-import { Form } from './components/form/Form';
+import  ProductList  from './components/ProductList/ProductList';
+import  Form  from './components/form/Form';
 
 function App() {
   const {onToggleButton, tg} = useTelegram();
