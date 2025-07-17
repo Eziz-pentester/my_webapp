5a9908be0078cb88cf101809334649bb4f92ce99
Eziz-pentester <ezizatabayew1598@gmail.com>
2025-07-17 12:36:06 +0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -2,17 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-import { BrowserRouter as brouserRouter } from 'react-router-dom';
-
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-  
   <React.StrictMode>
-    <BrouserRouter>
-     <App />
-    </BrouserRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
